02d3d8f75e15aa996d7012f40e0e7689d2ec4967
Gökhan <gn.ozdemir@gmail.com>
2023-12-19 16:21:21 +0300

    inserts add movie capability

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,6 +1,11 @@
 export const DELETE_MOVIE = "DELETE_MOVIE";
+export const ADD_MOVIE = "Film Ekler";
 
 export const deleteMovie = (id) => {
   // console.log("actionCreator Delete", id, typeof id);
   return { type: DELETE_MOVIE, payload: Number(id) };
+};
+
+export const addMovie = (movie) => {
+  return { type: ADD_MOVIE, payload: movie };
 };
